89aaf0f1b2537d17045502299dfa5b75187f1056
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2024-02-06 16:08:16 +0600

    init

--- conversationScript.js
+++ conversationScript.js
@@ -1,40 +1,213 @@
-const conversations = [{
-    conversationIdentifier: 'init',
-    triggerArray: ['why', 'sad', 'hurt'],
+// const conversations = [{
+//     conversationIdentifier: 'init',
+//     triggerArray: ['why', 'sad', 'hurt'],
+//     yes: {
+//         mood: 'sad',
+//         ssmlResponse: 'I am not being noticed',
+//         nextIdentifier: "con-2"
+//     },
+//     no: {
+//         mood: 'mad',
+//         ssmlResponse: 'Sorry, I am happy now',
+//         nextIdentifier: "con-2"
+//     }
+// }, {
+//     conversationIdentifier: 'con-2',
+//     triggerArray: ['who', 'not', 'notic'],
+//     yes: {
+//         mood: 'sad',
+//         ssmlResponse: 'Why are you like this?',
+//         nextIdentifier: "con-3"
+//     },
+//     no: {
+//         mood: 'mad',
+//         ssmlResponse: 'Sorry, I am happy now',
+//         nextIdentifier: "con-3"
+//     }
+// }, {
+//     conversationIdentifier: 'con-3',
+//     triggerArray: ['who', 'not', 'notic'],
+//     yes: {
+//         mood: 'happy',
+//         ssmlResponse: 'Why do you ignore me?',
+//         nextIdentifier: "con-4"
+//     },
+//     no: {
+//         mood: 'mad',
+//         ssmlResponse: 'Sorry, I am happy now',
+//         nextIdentifier: "con-4"
+//     }
+// }]
+
+// const conversations = [
+//   {
+//     conversationIdentifier: "con-1",
+//     triggerArray: ["enthusiastically", "ready", "fanciest", "world"],
+//     yes: {
+//       mood: "happy",
+//       ssmlResponse: "Let's have a tea party!",
+//       action: "giggling",
+//       nextIdentifier: "con-2",
+//     },
+//   },
+//   {
+//     conversationIdentifier: "con-2",
+//     triggerArray: ["okay", "yes", "sure", "let's go"],
+//     yes: {
+//       mood: "surprise",
+//       ssmlResponse: "I will go bring the tea set.",
+//       action: "giggling",
+//       nextIdentifier: "con-3",
+//     },
+//     no: {
+//       mood: "angry",
+//       ssmlResponse: "I really wanted to have the tea party now",
+//       action: "pouting",
+//       nextIdentifier: "con-4",
+//     },
+//   },
+//   {
+//     conversationIdentifier: "con-3",
+//     triggerArray: ["come", "with you", "join", "along"],
+//     yes: {
+//       mood: "neutral",
+//       ssmlResponse: "Can you help me bring the tea set down?",
+//       action: "crossing her arms",
+//       nextIdentifier: null,
+//     },
+//     no: {
+//       mood: "disgust",
+//       ssmlResponse: "Please come with me. I need your help.",
+//       action: "giggling",
+//       nextIdentifier: null,
+//     },
+//   },
+//   {
+//     conversationIdentifier: "con-4",
+//     triggerArray: ["okay", "yes", "sure", "let's go"],
+//     yes: {
+//       mood: "happy",
+//       ssmlResponse: "Yayyy!",
+//       action: "crossing her arms",
+//       nextIdentifier: null,
+//     },
+//     no: {
+//       mood: "sad",
+//       ssmlResponse: "Please?",
+//       action: "giggling",
+//       nextIdentifier: null,
+//     },
+//   },
+// ];
+
+const synonymsOne = [
+  "I don't wanna eat!",
+  "I not hungry!",
+  "I don't like food!",
+  "I don't feel like eating!",
+  "I not hungry right now!",
+  "I don't want any food!",
+  "I no want to eat!",
+  "I don't want dinner!",
+  "I not hungry for food!",
+  "I not hungry, mama!",
+];
+
+const synonymsTwo = [
+  "Mommy took my toy 'cause I didn't eat!",
+  "Mommy took my toy 'cause I didn't finish my food!",
+  "Mommy took away my toy 'cause I didn't eat!",
+  "Mommy took away my toy 'cause I didn't finish my food!",
+  "Mommy took my toy 'cause I didn't want to eat!",
+  "Mommy took away my toy 'cause I didn't want to eat!",
+  "Mommy took my toy 'cause I didn't like my food!",
+  "Mommy took away my toy 'cause I didn't like my food!",
+  "Mommy took my toy 'cause I didn't want my food!",
+  "Mommy took away my toy 'cause I didn't want my food!",
+];
+
+function randly(synonyms) {
+  console.log("🚀 ~ pickRandomSynonym ~ synonyms:", synonyms);
+  const randomIndex = Math.floor(Math.random() * synonyms.length);
+  console.log("🚀 ~ pickRandomSynonym ~ randomIndex:", randomIndex);
+  return synonyms[randomIndex];
+}
+
+const conversations = [
+  {
+    conversationIdentifier: "con-1",
+    triggerArray: ["ready", "What", "How", "Hi"],
     yes: {
-        mood: 'sad',
-        ssmlResponse: 'I am not being noticed',
-        nextIdentifier: "con-2"
+      mood: "angry",
+      ssmlResponse: randly(synonymsOne),
+      action: "Crying",
+      nextIdentifier: "con-2",
+    },
+  },
+  {
+    conversationIdentifier: "con-2",
+    triggerArray: ["Why", "What", "Happenned", "Who"],
+    yes: {
+      mood: "angry",
+      ssmlResponse: randly(synonymsTwo),
+      action: "Crying",
+      nextIdentifier: "con-1",
     },
     no: {
-        mood: 'mad',
-        ssmlResponse: 'Sorry, I am happy now',
-        nextIdentifier: "con-2"
-    }
-}, {
-    conversationIdentifier: 'con-2',
-    triggerArray: ['who', 'not', 'notic'],
+      mood: "angry",
+      ssmlResponse: ["I want my toy"],
+      action: "Crying",
+      nextIdentifier: "con-3",
+    },
+  },
+  {
+    conversationIdentifier: "con-3",
+    triggerArray: ["let's", "make", "deal", "come to"],
     yes: {
-        mood: 'sad',
-        ssmlResponse: 'Why are you like this?',
-        nextIdentifier: "con-3"
+      mood: "angry",
+      ssmlResponse: ["What deal? I don't want any deal"],
+      action: "crossing her arms",
+      nextIdentifier: "con-4",
     },
     no: {
-        mood: 'mad',
-        ssmlResponse: 'Sorry, I am happy now',
-        nextIdentifier: "con-3"
-    }
-}, {
-    conversationIdentifier: 'con-3',
-    triggerArray: ['who', 'not', 'notic'],
+      mood: "angry",
+      ssmlResponse: ["I want my toy back!"],
+      action: "giggling",
+      nextIdentifier: "con-4",
+    },
+  },
+  {
+    conversationIdentifier: "con-4",
+    triggerArray: ["Give", "toy", "eat", "food", "lunch"],
     yes: {
-        mood: 'happy',
-        ssmlResponse: 'Why do you ignore me?',
-        nextIdentifier: "con-4"
+      mood: "angry",
+      ssmlResponse: ["No! You first give me my toy back!"],
+      action: "crossing her arms",
+      nextIdentifier: "con-5",
     },
     no: {
-        mood: 'mad',
-        ssmlResponse: 'Sorry, I am happy now',
-        nextIdentifier: "con-4"
-    }
-}]
+      mood: "angry",
+      ssmlResponse: ["Why did she take my toy! I won't eat my lunch"],
+      action: "giggling",
+      nextIdentifier: "con-5",
+    },
+  },
+  //   {
+  //     conversationIdentifier: "con-5",
+  //     triggerArray: ["mad", "angry", "crazy"],
+  //     yes: {
+  //       mood: "angry",
+  //       ssmlResponse: "Yes",
+  //       action: "crossing her arms",
+  //       nextIdentifier: null,
+  //     },
+  //     no: {
+  //       mood: "angry",
+  //       ssmlResponse: "No",
+  //       action: "giggling",
+  //       nextIdentifier: "con-5",
+  //     },
+  //   },
+];
+
+// ? When we run out of script we can look through all the trigger array to find the best match with the current sentence and then use that conversationIdentifier
--- script2.js
+++ script2.js
@@ -1,147 +1,111 @@
 
-function doesWordExist(userJustSaid, triggerArray) {
-    console.log("🚀 ~ doesWordExist ~ userJustSaid:", userJustSaid)
-    let _ujs_lo = userJustSaid.toLowerCase();
-    for (var i = triggerArray.length - 1; i >= 0; i--) {
-        let _t = triggerArray[i];
-        if (_ujs_lo.search(_t) != -1) {
-            //   frame="1a_1";
-            //   frameOncer=0;
-            return true;
-        }
+const doesWordExist = (userJustSaid, triggerArray) => {
+  console.log("🚀 ~ doesWordExist ~ userJustSaid:", userJustSaid);
+  const userJustSaidLower = userJustSaid.toLowerCase();
+  for (let i = triggerArray.length - 1; i >= 0; i--) {
+    const triggerLower = triggerArray[i].toLowerCase();
+    if (userJustSaidLower.includes(triggerLower)) {
+      return true;
     }
-    return false
-}
+  }
+  return false;
+};
 
-let currentConversationIdentifier = 'init'
-
-function addChat(input, product) {
-    const mainDiv = document.getElementById("message-section");
+function addChat(input, product, init) {
+  const { mood, ssmlResponse, action } = product;
+  const mainDiv = document.getElementById("message-section");
+  if (!init) {
     let userDiv = document.createElement("div");
     userDiv.id = "user";
     userDiv.classList.add("message");
     userDiv.innerHTML = `<span id="user-response">${input}</span>`;
     mainDiv.appendChild(userDiv);
+  }
 
-    let botDiv = document.createElement("div");
-    botDiv.id = "bot";
-    botDiv.classList.add("message");
-    botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
-    mainDiv.appendChild(botDiv);
-    var scroll = document.getElementById("message-section");
-    scroll.scrollTop = scroll.scrollHeight;
+  let botDiv = document.createElement("div");
+  botDiv.id = "bot";
+  botDiv.classList.add("message");
+  botDiv.innerHTML = `<span id="bot-response">${
+    ssmlResponse
+  }</span>`;
+  mainDiv.appendChild(botDiv);
+  var scroll = document.getElementById("message-section");
+  scroll.scrollTop = scroll.scrollHeight;
+
+  document.getElementById("mood").innerText = mood;
+  document.getElementById("action").innerText = action;
 }
 
 function sendMessage() {
-    const inputField = document.getElementById("input");
-    let input = inputField.value.trim();
-    input != "" && outputTest(input);
-    inputField.value = "";
+  const inputField = document.getElementById("input");
+  let input = inputField.value.trim();
+  input != "" && outputTest(input, false);
+  inputField.value = "";
 }
-let triggerArray = ['why', 'sad', 'hurt']
+let triggerArray = ["why", "sad", "hurt"];
 
-async function output(input) {
-    if (conversationIdentifier == 'init') {
-        if (doesWordExist(input)) {
-            let response = 'I am not being noticed'
-            addChat(input, response)
-            let text = generateSSMLForMood(response, "mad")
-            console.log("🚀 ~ output ~ text:", text)
-            const { AudioStream, visemes } = await speakText(text)
-            streamAudioData(AudioStream);
+let currentConversationIdentifier = "con-1";
 
-            conversationIdentifier = 'con-2' // ? strings can be used here to redirect to another flow
-        }
-        else {
-            let response = 'Sorry, I am happy now'
-            addChat(input, response)
-            let text = generateSSMLForMood(response, "happy")
-            console.log("🚀 ~ output ~ text:", text)
-            const { AudioStream, visemes } = await speakText(text)
-            streamAudioData(AudioStream);
-            conversationIdentifier = 'con-2'
-        }
-    }
-    else if (conversationIdentifier == 'con-2') {
-        triggerArray = ['who', 'not', 'notic']
-        if (doesWordExist(input)) {
-            let response = 'Why are you like this?'
-            addChat(input, response)
-            let text = generateSSMLForMood(response, "happy")
-            const { AudioStream, visemes } = await speakText(text)
-            streamAudioData(AudioStream);
-            conversationIdentifier = 'con-3'
-        }
-        else {
-            let response = 'Sorry, I am happy now'
-            addChat(input, response)
-            conversationIdentifier = 'con-3'
-        }
-    }
-    else if (conversationIdentifier == 'con-3') {
-        triggerArray = ['who', 'not', 'notic']
-        if (doesWordExist(input)) {
-            let response = 'Why do you ignore me?'
-            addChat(input, response)
-            let text = generateSSMLForMood(response, "sad")
-            const { AudioStream, visemes } = await speakText(text)
-            streamAudioData(AudioStream);
-            conversationIdentifier = 'con-3'
-        }
-        else {
-            let response = 'Sorry, I am happy now'
-            addChat(input, response)
+window.onload = function () {
+  outputTest("ready", true);
+};
 
-        }
-    }
-}
-async function outputTest(input) {
-    const { triggerArray, yes, no } = conversations.find(({ conversationIdentifier }) => conversationIdentifier == currentConversationIdentifier)
-    if (doesWordExist(input, triggerArray)) {
-        addChat(input, yes.ssmlResponse)
-        let text = generateSSMLForMood(yes.ssmlResponse, yes.mood)
-        const { AudioStream, visemes } = await speakText(text)
-        streamAudioData(AudioStream);
-        currentConversationIdentifier = yes.nextIdentifier
-    }
-    else {
-        addChat(input, no.ssmlResponse)
-        let text = generateSSMLForMood(no.ssmlResponse, no.mood)
-        const { AudioStream, visemes } = await speakText(text)
-        streamAudioData(AudioStream);
-        currentConversationIdentifier = no.nextIdentifier
-    }
+async function outputTest(input, init) {
+  const { triggerArray, yes, no } = conversations.find(
+    ({ conversationIdentifier }) =>
+      conversationIdentifier == currentConversationIdentifier
+  );
+  if (doesWordExist(input, triggerArray)) {
+    console.log("🚀 ~ outputTest ~ doesWordExist: if");
+    addChat(input, yes, init);
+    let text = generateSSMLForMood(yes.ssmlResponse, yes.mood);
+    const { AudioStream, visemes } = await speakText(text);
+    streamAudioData(AudioStream);
+    currentConversationIdentifier = yes.nextIdentifier;
+  } else {
+    console.log("🚀 ~ outputTest ~ doesWordExist: else");
+    addChat(input, no, init);
+    let text = generateSSMLForMood(no.ssmlResponse, no.mood);
+    const { AudioStream, visemes } = await speakText(text);
+    streamAudioData(AudioStream);
+    currentConversationIdentifier = no.nextIdentifier;
+  }
 }
 
 function addPollyTagsForVoiceEmotion(text, mood) {
-    switch (mood) {
-        case "happy":
-            return '<speak><prosody volume="+20dB">Mary had <break time="300ms" />a little <mark name="animal" />lamb</prosody></speak>'
-        case "sad":
-            return text
-        case "mad":
-            return text
-        default:
-            return text
-    }
+  switch (mood) {
+    case "happy":
+      return '<speak><prosody volume="+20dB">Mary had <break time="300ms" />a little <mark name="animal" />lamb</prosody></speak>';
+    case "sad":
+      return text;
+    case "mad":
+      return text;
+    default:
+      return text;
+  }
 }
-
 
 function generateSSMLForMood(text, mood) {
-    switch (mood) {
-        case "happy":
-            return `<speak><prosody volume="+20dB" rate="fast">${text}</prosody></speak>`;
-        case "sad":
-            return `<speak><prosody volume="-10dB" rate="slow" >${text}</prosody></speak>`;
-        case "mad":
-            return `<speak><amazon:breath duration="long" volume="x-loud"/><prosody volume="+20dB" pitch="high">${text}</prosody></speak>`;
-        default:
-            return `<speak>${text}</speak>`;
-    }
+  switch (mood) {
+    case "happy":
+      return `<speak><prosody volume="+20dB" rate="fast">${text}</prosody></speak>`;
+    case "sad":
+      return `<speak><prosody volume="-10dB" rate="slow">${text}</prosody></speak>`;
+    case "angry":
+      return `<speak><amazon:breath duration="long" volume="x-loud"/><prosody volume="+20dB" pitch="high">${text}</prosody></speak>`;
+    case "disgusted":
+      return `<speak><voice name="Joanna"><prosody rate="slow">${text}</prosody></voice></speak>`;
+    case "surprised":
+      return `<speak><prosody pitch="x-high">${text}</prosody></speak>`;
+    case "frightened":
+      return `<speak><amazon:breath duration="long"/><prosody rate="slow">${text}</prosody></speak>`;
+    default:
+      return `<speak>${text}</speak>`;
+  }
 }
 
-
-{/* 
+{
+  /* 
 Voice Acting
 https://docs.aws.amazon.com/polly/latest/dg/supportedtags.html
 <emphasis level="strong">Strong emphasis</emphasis>
@@ -150,4 +114,5 @@ https://docs.aws.amazon.com/polly/latest/dg/supportedtags.html
 <prosody volume="soft">Soft speech</prosody>
 <prosody rate="fast">Fast speech</prosody>
 <prosody rate="slow">Slow speech</prosody> 
-*/}
+*/
+}
