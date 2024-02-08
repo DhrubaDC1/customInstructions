62807f35031d64265732b2c9064b0c773d108976
Showrov Islam <63032286+showrov29@users.noreply.github.com>
2024-02-08 17:51:37 +0600

    replicate cris's coversation

--- script2.js
+++ script2.js
@@ -1,117 +1,128 @@
 const doesWordExist = (userJustSaid, triggerArray) => {
-  console.log("🚀 ~ doesWordExist ~ userJustSaid:", userJustSaid);
-  const userJustSaidLower = userJustSaid.toLowerCase();
-  for (let i = triggerArray.length - 1; i >= 0; i--) {
-    const triggerLower = triggerArray[i].toLowerCase();
-    if (userJustSaidLower.includes(triggerLower)) {
-      return true;
-    }
-  }
-  return false;
+	console.log("🚀 ~ doesWordExist ~ userJustSaid:", userJustSaid);
+	const userJustSaidLower = userJustSaid.toLowerCase();
+	for (let i = triggerArray.length - 1; i >= 0; i--) {
+		const triggerLower = triggerArray[i].toLowerCase();
+		if (userJustSaidLower.includes(triggerLower)) {
+			return true;
+		}
+	}
+	return false;
 };
 
 function randly(synonyms) {
-  const randomIndex = Math.floor(Math.random() * synonyms.length);
-  console.log("🚀 ~ pickRandomSynonym ~ randomIndex:", randomIndex, synonyms);
-  return synonyms[randomIndex];
+	const randomIndex = Math.floor(Math.random() * synonyms.length);
+	console.log("🚀 ~ pickRandomSynonym ~ randomIndex:", randomIndex, synonyms);
+	return synonyms[randomIndex];
 }
 
 function addChat(input, product, init) {
-  const { mood, ssmlResponse, action } = product;
-  const mainDiv = document.getElementById("message-section");
-  if (!init) {
-    let userDiv = document.createElement("div");
-    userDiv.id = "user";
-    userDiv.classList.add("message");
-    userDiv.innerHTML = `<span id="user-response">${input}</span>`;
-    mainDiv.appendChild(userDiv);
-  }
+	const { mood, ssmlResponse, action } = product;
+	const mainDiv = document.getElementById("message-section");
+	if (!init) {
+		let userDiv = document.createElement("div");
+		userDiv.id = "user";
+		userDiv.classList.add("message");
+		userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+		mainDiv.appendChild(userDiv);
+	}
 
-  let botDiv = document.createElement("div");
-  botDiv.id = "bot";
-  botDiv.classList.add("message");
-  botDiv.innerHTML = `<span id="bot-response">${ssmlResponse}</span>`;
-  mainDiv.appendChild(botDiv);
-  var scroll = document.getElementById("message-section");
-  scroll.scrollTop = scroll.scrollHeight;
+	let botDiv = document.createElement("div");
+	botDiv.id = "bot";
+	botDiv.classList.add("message");
+	botDiv.innerHTML = `<span id="bot-response">${ssmlResponse}</span>`;
+	mainDiv.appendChild(botDiv);
+	var scroll = document.getElementById("message-section");
+	scroll.scrollTop = scroll.scrollHeight;
 
-  document.getElementById("mood").innerText = mood;
-  document.getElementById("action").innerText = action;
+	document.getElementById("mood").innerText = mood;
+	document.getElementById("action").innerText = action;
 }
 
 function sendMessage() {
-  const inputField = document.getElementById("input");
-  let input = inputField.value.trim();
-  input != "" && outputTest(input, false);
-  inputField.value = "";
+	const inputField = document.getElementById("input");
+	let input = inputField.value.trim();
+	input != "" && outputTest(input, false);
+	inputField.value = "";
 }
 let triggerArray = ["why", "sad", "hurt"];
 
 let currentConversationIdentifier = "con-1";
 
 window.onload = function () {
-  outputTest("ready", true);
+	outputTest("ready", true);
 };
 
 async function outputTest(input, init) {
-  const { triggerArray, yes, no } = conversations.find(
-    ({ conversationIdentifier }) =>
-      conversationIdentifier == currentConversationIdentifier
-  );
-  if (doesWordExist(input, triggerArray)) {
-    let prevYSsml = { ...yes };
-    prevYSsml.ssmlResponse = randly(prevYSsml.ssmlResponse);
+	const { triggerArray, yes, no } = conversations.find(
+		({ conversationIdentifier }) =>
+			conversationIdentifier == currentConversationIdentifier
+	);
+	if (doesWordExist(input, triggerArray)) {
+		let prevYSsml = { ...yes };
+		prevYSsml.ssmlResponse = replaceUserJustSaid(
+			randly(prevYSsml.ssmlResponse),
+			input
+		);
 
-    addChat(input, prevYSsml, init);
-    let text = generateSSMLForMood(prevYSsml.ssmlResponse, yes.mood);
-    const { AudioStream, visemes } = await speakText(text);
-    streamAudioData(AudioStream);
-    currentConversationIdentifier = yes.nextIdentifier;
-  } else {
-    let prevNSsml = { ...no };
-    prevNSsml.ssmlResponse = randly(prevNSsml.ssmlResponse);
-    addChat(input, prevNSsml, init);
-    let text = generateSSMLForMood(prevNSsml.ssmlResponse, no.mood);
-    const { AudioStream, visemes } = await speakText(text);
-    streamAudioData(AudioStream);
-    currentConversationIdentifier = no.nextIdentifier;
-  }
+		addChat(input, prevYSsml, init);
+		let text = generateSSMLForMood(prevYSsml.ssmlResponse, yes.mood);
+		const { AudioStream, visemes } = await speakText(text);
+		streamAudioData(AudioStream);
+		currentConversationIdentifier = yes.nextIdentifier;
+	} else {
+		let prevNSsml = { ...no };
+		prevNSsml.ssmlResponse = replaceUserJustSaid(
+			randly(prevNSsml.ssmlResponse),
+			input
+		);
+		addChat(input, prevNSsml, init);
+		let text = generateSSMLForMood(prevNSsml.ssmlResponse, no.mood);
+		const { AudioStream, visemes } = await speakText(text);
+		streamAudioData(AudioStream);
+		currentConversationIdentifier = no.nextIdentifier;
+	}
 }
 
 function addPollyTagsForVoiceEmotion(text, mood) {
-  switch (mood) {
-    case "happy":
-      return '<speak><prosody volume="+20dB">Mary had <break time="300ms" />a little <mark name="animal" />lamb</prosody></speak>';
-    case "sad":
-      return text;
-    case "mad":
-      return text;
-    default:
-      return text;
-  }
+	switch (mood) {
+		case "happy":
+			return '<speak><prosody volume="+20dB">Mary had <break time="300ms" />a little <mark name="animal" />lamb</prosody></speak>';
+		case "sad":
+			return text;
+		case "mad":
+			return text;
+		default:
+			return text;
+	}
 }
 
 function generateSSMLForMood(text, mood) {
-  switch (mood) {
-    case "happy":
-      return `<speak><prosody volume="+20dB" rate="fast">${text}</prosody></speak>`;
-    case "sad":
-      return `<speak><prosody volume="-10dB" rate="slow">${text}</prosody></speak>`;
-    case "angry":
-      return `<speak><amazon:breath duration="long" volume="x-loud"/><prosody volume="+20dB" pitch="high">${text}</prosody></speak>`;
-    case "disgusted":
-      return `<speak><voice name="Joanna"><prosody rate="slow">${text}</prosody></voice></speak>`;
-    case "surprised":
-      return `<speak><prosody pitch="x-high">${text}</prosody></speak>`;
-    case "frightened":
-      return `<speak><amazon:breath duration="long"/><prosody rate="slow">${text}</prosody></speak>`;
-    default:
-      return `<speak>${text}</speak>`;
-  }
+	switch (mood) {
+		case "happy":
+			return `<speak><prosody volume="+20dB" rate="fast">${text}</prosody></speak>`;
+		case "sad":
+			return `<speak><prosody volume="-10dB" rate="slow">${text}</prosody></speak>`;
+		case "angry":
+			return `<speak><amazon:breath duration="long" volume="x-loud"/><prosody volume="+20dB" pitch="high">${text}</prosody></speak>`;
+		case "disgusted":
+			return `<speak><voice name="Joanna"><prosody rate="slow">${text}</prosody></voice></speak>`;
+		case "surprised":
+			return `<speak><prosody pitch="x-high">${text}</prosody></speak>`;
+		case "frightened":
+			return `<speak><amazon:breath duration="long"/><prosody rate="slow">${text}</prosody></speak>`;
+		default:
+			return `<speak>${text}</speak>`;
+	}
+}
+function replaceUserJustSaid(sentence, replacement) {
+	let str = sentence.replace(/#userJustSaid/g, replacement);
+	console.log("🚀 ~ replaceUserJustSaid ~ str:", str);
+	return str;
 }
 
 {
-  /* 
+	/* 
   Voice Acting
   https://docs.aws.amazon.com/polly/latest/dg/supportedtags.html
   <emphasis level="strong">Strong emphasis</emphasis>
